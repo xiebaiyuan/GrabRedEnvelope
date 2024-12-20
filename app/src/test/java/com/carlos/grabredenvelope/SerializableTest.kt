d26b0f51f56c5c37e6cfaaf930e3b3a2015733f3
xiebaiyuan <xiebaiyuan@139.com>
2024-12-21 02:30:36 +0800

    升级工具链。 去除乱七八糟的。

--- app/src/test/java/com/carlos/grabredenvelope/SerializableTest.kt
+++ app/src/test/java/com/carlos/grabredenvelope/SerializableTest.kt
@@ -1,26 +1,26 @@
-package com.carlos.grabredenvelope
-
-import com.carlos.grabredenvelope.dao.WechatControlVO
-import kotlinx.serialization.json.Json
-import org.junit.Test
-
-/**
- * Created by Carlos on 2019/2/21.
- */
-class SerializableTest {
-
-    @Test
-    fun testSerializable() {
-
-        val wechatControlVO = WechatControlVO()
-
-        val json = Json.encodeToString(WechatControlVO.serializer(), wechatControlVO)
-        println("json:$json")
-
-
-        val data = Json.decodeFromString(WechatControlVO.serializer(), json)
-        println("isMonitor:${data.isMonitorChat}")
-
-    }
-
-}
+//package com.carlos.grabredenvelope
+//
+//import com.carlos.grabredenvelope.dao.WechatControlVO
+//import kotlinx.serialization.json.Json
+//import org.junit.Test
+//
+///**
+// * Created by Carlos on 2019/2/21.
+// */
+//class SerializableTest {
+//
+//    @Test
+//    fun testSerializable() {
+//
+//        val wechatControlVO = WechatControlVO()
+//
+//        val json = Json.encodeToString(WechatControlVO.serializer(), wechatControlVO)
+//        println("json:$json")
+//
+//
+//        val data = Json.decodeFromString(WechatControlVO.serializer(), json)
+//        println("isMonitor:${data.isMonitorChat}")
+//
+//    }
+//
+//}
